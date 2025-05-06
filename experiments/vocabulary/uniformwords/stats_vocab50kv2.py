a7e7573e53d6807b415998852d79ccb13a12b5e4
angelolmn <e.angelolm#go.ugr.es>
2025-05-06 12:16:21 +0200

    Experiments with uniform vocabulary added, iamges are not added for size (ref #26)

---
+++ experiments/vocabulary/uniformwords/stats_vocab50kv2.py
@@ -0,0 +1,23 @@
+from hitsbe import vocabulary2
+import matplotlib.pyplot as plt
+
+v50k = vocabulary2.Vocabulary("experiments/vocabulary/uniformwords/50kv2.vocab")
+
+fig, axes = plt.subplots(nrows=32, ncols=4, figsize=(16, 64))
+
+axes = axes.flatten()
+
+for i, w in enumerate(v50k.words):
+    ax = axes[i]
+    x = list(range(8))
+
+    for word in w:
+        ax.plot(x, word, alpha = 0.2)
+
+    ax.set_title(f"Monotonia {i} -  {format(i, '07b')} - {len(w)} words")
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+plt.tight_layout()
+
+plt.savefig("experiments/vocabulary/uniformwords/50kv2_dist.png")
