8eca5aae8e4272fcf960a8efff0f2f14193f7e6b
angelolmn <e.angelolm#go.ugr.es>
2025-05-13 19:27:38 +0200

    feat: all the featured added (ref #27)

--- hitsbe/hitsbe.py
+++ hitsbe/hitsbe.py
@@ -1,32 +1,38 @@
-import numpy as np
 import torch
 import torch.nn as nn
-import scipy.stats as stats
 import pywt 
 import math
 
-from .vocabulary import Vocabulary
+from .vocabulary2 import Vocabulary
 
 
 class Hitsbe(nn.Module):
-    def __init__(self, primal=True):
+    def __init__(self, filename=""):
         super(Hitsbe, self).__init__()
-        self.vocabulary = Vocabulary(primal)
-        self.threshold = 0.95
-        self.size = 1024
-        self.cell_size = 8
-        self.dim_seq = self.size // self.cell_size
 
-        self.register_buffer("att_mask", torch.ones(self.dim_seq, dtype=torch.int32))
+        self.vocabulary = Vocabulary(file=filename)
+        
+        vocab_dist = torch.tensor([len(w) for  w in self.vocabulary])
+        vocab_dist = torch.cumsum(vocab_dist, dim=0)
+        vocab_dist = torch.roll(vocab_dist, shifts = 1)
+        vocab_dist[0] = 0
+        # It contains the cumulative sums of all the previous monotonicity classes
+        self.register_buffer("vocabulary_dist", vocab_dist) # No trainable
+
+        
+        self.ts_len = 1024
+        self.segment_len = 8
+
+        self.dim_seq = self.ts_len // self.segment_len
 
         # 768 for BERT
         self.dim_model = 768
-
+        
         # Number of levels for the Haar transform
-        self.nhaar_level = int(np.log2(self.dim_seq)) + 1
-
+        self.nhaar_level = int(torch.log2(torch.tensor(self.dim_seq, dtype=torch.float32)).item())
+        
         self.word_emb_matrix = nn.Embedding(len(self.vocabulary), self.dim_model)
-        self.haar_emb_matrix = nn.Parameter(torch.randn(int(self.nhaar_level), self.dim_model))
+        self.haar_emb_matrix = nn.Parameter(torch.randn(self.nhaar_level + 1, self.dim_model))
 
         # Positional encoding.
         # The sinusoidal version is the simplest to implement initially.
@@ -40,202 +46,143 @@ class Hitsbe(nn.Module):
         pos_emb = torch.zeros(self.dim_seq, self.dim_model)
         pos_emb[:, 0::2] = torch.sin(position * div_term)
         pos_emb[:, 1::2] = torch.cos(position * div_term)
-        self.register_buffer("pos_emb_matrix", pos_emb)
+        self.register_buffer("pos_emb_matrix", pos_emb) # No trainable
 
 
     def _adjust(self, X):
-        "Centers the array X"
-        if not torch.is_tensor(X):
-            X = torch.tensor(X, dtype=torch.float)
+        "Centers each row of the batch X."
+        assert isinstance(X, torch.Tensor), "Expected X to be a torch.Tensor"
 
-        n = len(X)
-        
-        # Calculate the starting index so that X is centered
-        start = (self.size - n) // 2
+        device = next(self.parameters()).device
+        batch_size = X.size(0)
 
-        # Create a zero array with the same data type as X
-        X_adj = torch.zeros(self.size, dtype=X.dtype, device=X.device)
-        
-        # Place X into the center of X_adj
-        X_adj[start:start + n] = X
+        X_adj = torch.zeros(batch_size, self.ts_len, device=device)
+        att_mask = torch.zeros(batch_size, self.dim_seq, dtype=torch.long, device=device)        
 
-        # Config att_mask
-        start_att_mask = start//self.cell_size
-        end_att_mask = (start + n)//self.cell_size
 
-        self.att_mask[:start_att_mask] = 0
-        self.att_mask[end_att_mask + 1:] = 0 
+        for i,x in enumerate(X):
+            N = x.size(0)
 
-        return X_adj
-        
-    @staticmethod
-    def cross_corr(serie, word):
-        """
-        Computes the correlation of the word with each segment of the time series of the same size.
-        Returns a list of floating-point correlations.
-        """
-        if not torch.is_tensor(serie):
-            serie = torch.tensor(serie, dtype=torch.float)
-        else:
-            serie = serie.float()
+            if N < self.ts_len:
+                # Calculate the starting index so that X is centered
+                start = (self.ts_len - N) // 2
+                end = start + N
+
+                # Place X into the center of X_adj
+                X_adj[i, start:end] = x
+
+                # Config att_mask
+                start_att_mask = start//self.segment_len
+                end_att_mask = (start + N)//self.segment_len # include the last segment
+
+                att_mask[i, start_att_mask:end_att_mask] = 1
+            elif N > self.ts_len:
+                X_adj[i] = x[:self.ts_len]
+                att_mask[i] = 1
+            else:
+                X_adj[i] = x
+                att_mask[i] = 1
             
-        if not torch.is_tensor(word):
-            # Move the word tensor to the same device as the serie
-            word = torch.tensor(word, dtype=torch.float, device=serie.device)
+        return X_adj, att_mask
+        
+    def get_best_approach(self, segment, return_index = False):
+        device = next(self.parameters()).device
+        segment_bit_index = self.vocabulary._compute_index(segment)
+
+        smin = torch.min(segment)
+        smax = torch.max(segment)
+        # (segment_len)
+        segment_norm = (segment - smin) / (smax - smin + 1e-8)
+
+        # (N_words, segment_len)
+        vocab_tensor = torch.tensor(self.vocabulary[segment_bit_index], dtype=segment_norm.dtype, device= device)
+
+        distances = torch.abs(vocab_tensor - segment_norm).sum(dim=1)
+
+        best_word_index = torch.argmin(distances).item()
+
+        if not return_index:
+            embed_index = self.vocabulary_dist[segment_bit_index] + best_word_index
+            return self.word_emb_matrix(embed_index)
         else:
-            word = word.float().to(serie.device)
+            return segment_bit_index, best_word_index
 
-        N = len(serie)  # Series length (ideally 2^10)
-        M = len(word)   # Word length (ideally 2^3)          N / M = 128 = 2^7
+    def compute_word_embedding(self, X, att_mask, return_index = False):
+        """
+        Returns the sequence with the corresponding words and their values.
+        """
+        device = next(self.parameters()).device
 
-        correlations = []  # Will store the Pearson coefficients
+        batch_size = X.size(0)
 
-        std_word = torch.std(word, unbiased=False)
-        mean_word = torch.mean(word)
+        if not return_index:
+            batch_sequence = torch.zeros((batch_size, self.dim_seq, self.dim_model), device = device)
+        else:
+            batch_sequence = torch.zeros((batch_size, self.dim_seq, 2), device = device)
 
-        for i in range(0, N - M + 1, M):
-            subseq = serie[i:i + M]  # Extract the subsequence of size M
-            std_subseq = torch.std(subseq, unbiased=False)
+        for i, x in enumerate(X):
+            mask = att_mask[i]
+            valid_index = torch.nonzero(mask, as_tuple=True)[0]
 
-            if std_word == 0 or std_subseq == 0:
-                r = 0.0 
-            else:
-                mean_subseq = torch.mean(subseq)
-                cov = torch.mean((word - mean_word) * (subseq - mean_subseq))
-                r = (cov / (std_word * std_subseq)).item()
+            for k in valid_index:
+                start = k*self.segment_len
+                end = start + self.segment_len
+                segment = x[start:end]
+                batch_sequence[i, k] = self.get_best_approach(segment, return_index)
+    
+        return batch_sequence    
 
-            correlations.append(float(r))
+    def compute_haar_embedding(self, X, att_mask):
+        device = next(self.parameters()).device
+
+        # Move to CPU to copmpute all the coefficients
+        X_np = X.detach().cpu().numpy()
+
+        coeffs_list = []
+        for i, x in enumerate(X_np):
+            # [D1, D2, D3,...] Di = 2*D{i-1}, D1 = 1
+            haar_coeffs = pywt.wavedec(x, 'haar')[:self.nhaar_level+1]
+
+            # It is expected that the last Di level has dim_seq elements 
+            coeffs_tensor = [torch.from_numpy(hc) for hc in haar_coeffs]
+
+            # We convert the coeffs into a matrix (nhaar_level, dim_seq)
+            haar_matrix = [hc.repeat_interleave(self.dim_seq // hc.size(0)) for hc in coeffs_tensor]
             
-        return correlations
-
-
-    def get_sequence(self, X):
-        """
-        Returns the sequence with the corresponding words and their correlations.
-        """
-        seq = [(-np.inf, -np.inf) for _ in range(self.dim_seq)]
-
-        if len(X) != self.size:
-            X = self._adjust(X)
+            haar_matrix = torch.stack(haar_matrix)
+            
+            # Alternate dimensions to fit in coeffs_batch. Now each segment of each sequence has
+            # its corresponding nhaarl_levels coeff 
+            coeffs_list.append(haar_matrix.transpose(0, 1))
         
-        # Fill the sequence with the corresponding words
-        for i, w in enumerate(self.vocabulary):
-            corr = self.cross_corr(X, w)
-            for j, c in enumerate(corr):
-                if seq[j][1] < c and c > self.threshold:
-                    seq[j] = (i, c)
+        # Move to device
+        coeffs_batch = torch.stack(coeffs_list).to(device=device)
 
+        coeffs_masked = coeffs_batch*att_mask.unsqueeze(-1)
+                
+        # coeffs_masked (batch_size, dim_seq, nhaar_level)
+        # haar_emb_matrix = (nhaar_level, dim_model)
+        coeffs_embebed = coeffs_masked @ self.haar_emb_matrix
 
-        return seq
-
-    def compute_word_embedding(self, seq_mask, seq):
-        """
-        Computes the word embeddings for each segment indicated as valid by the sequence mask
-        """
-        word_emb = [] 
-        
-        # Iterate over each element in seq_mask along with its index
-        for i, m in enumerate(seq_mask):
-            # If the mask value is truthy (e.g., 1 or True), then the segment is valid
-            if m:
-                # Retrieve the word embedding for the segment:
-                # seq[i][0] is used as an index into the word embedding module
-                idx = seq[i][0]
-                # Create the index tensor on the same device as seq_mask
-                idx_tensor = torch.tensor([idx], device=seq_mask.device)
-                # Call the embedding module with a tensor index and remove the extra batch dimension
-                emb = self.word_emb_matrix(idx_tensor).squeeze(0)
-
-                word_emb.append(emb)
-
-            else:
-                word_emb.append(torch.zeros(self.dim_model,
-                                            device=self.word_emb_matrix.weight.device, 
-                                            dtype=self.word_emb_matrix.weight.dtype)
-                                )   
-        
-        # Return the list of computed word embeddings
-        return word_emb 
-
-
-    def compute_haar_embedding(self, seq_mask, haar_coeffs):
-        """
-        Computes the Haar embedding for each segment indicated as valid by the sequence mask
-        """
-        haar_coeff_to_embed = []
-        
-        for i, m in enumerate(seq_mask):
-            if m:  
-                hc = [] 
-                # For each level in the Haar coefficients,
-                # append the corresponding value to haar_coeff_to_embed.
-                #
-                # If i is the segment index, d is the dimension of the sequence
-                # (d is equal to the number of coefficients in our last
-                # level of the Haar transform calculated) and n is the number
-                # of coefficients in the current Haar level,
-                # then the coefficient associated with the word is
-                #                  i // (d / n)
-                # Note: d mod n = 0
-                for hlevel in range(self.nhaar_level):
-                    index = int(i // (self.dim_seq / len(haar_coeffs[hlevel])))
-                    # Append the coefficient from the current Haar level
-                    hc.append(haar_coeffs[hlevel][index])
-
-                # Append the list of coefficients for this valid segment
-                haar_coeff_to_embed.append(torch.tensor(hc, dtype=torch.float, device=self.haar_emb_matrix.device))
-        
-            else:
-                haar_coeff_to_embed.append(torch.zeros(self.nhaar_level, dtype=torch.float, device=self.haar_emb_matrix.device))
-
-        # Convert the list of embeddings to a tensor
-        haar_coeff_to_embed_tensor = torch.stack(haar_coeff_to_embed, dim=0)
-        # Compute the dot product between the matrix of Haar coefficients and the Haar embedding matrix
-        haar_embed = torch.matmul(haar_coeff_to_embed_tensor, self.haar_emb_matrix)        
-        
-        return haar_embed
+        return coeffs_embebed
 
 
     def get_embedding(self, X):
-    
-        final_embeddings = []
+        assert isinstance(X, torch.Tensor), "Expected X to be a torch.Tensor"
 
-        for x in X:
-            # Ensure x is a torch tensor so that we can use .device
-            if not torch.is_tensor(x):
-                x = torch.tensor(x, dtype=torch.float)
+        device = next(self.parameters()).device
+        X = X.to(device)
 
-            # Move x to the same device as the model parameters (e.g., GPU)
-            x = x.to(self.word_emb_matrix.weight.device)
+        X_adj, att_mask = self._adjust(X)
 
-                
-            if len(x) != self.size:
-                x = self._adjust(x)
+        # Get the sequences (segments and correlations)
+        sequence_embed = self.compute_word_embedding(X_adj, att_mask)
+        
+        haar_embed = self.compute_haar_embedding(X_adj, att_mask)
 
-            # Get the sequence (segments and correlations)
-            seq = self.get_sequence(x)
-            seq_mask = torch.tensor([1 if np.isfinite(w[0]) else 0 for w in seq], dtype=torch.long, device=x.device)
-    
-            word_embed = self.compute_word_embedding(seq_mask, seq)
-            
-            # Compute the Haar wavelet decomposition of x and select levels 1 to nhaar_level
-            haar_coeffs_tuple = pywt.wavedec(x.cpu().numpy(), 'haar')[1:int(self.nhaar_level)+1]
-            haar_coeffs = [torch.tensor(arr, dtype=torch.float, device=self.haar_emb_matrix.device) for arr in haar_coeffs_tuple]
-            # compute_haar_embedding returns a tensor of shape (dim_seq, dim_model)
-            haar_embed_tensor = self.compute_haar_embedding(seq_mask, haar_coeffs)
-            # Convert each row of the tensor into a tensor (to be compatible with word and positional embeddings)
-            haar_embed = [row.to(x.device).type(word_embed[0].dtype) for row in haar_embed_tensor]
+        batchts_embebed = sequence_embed + haar_embed + self.pos_emb_matrix
 
-            # Move pos_emb_matrix to the same device as x
-            pos_emb = self.pos_emb_matrix.to(x.device)
-
-            # For each segment, sum the word, positional and Haar embeddings.
-            instance_embeddings = [w + p + h for w, p, h in zip(word_embed, pos_emb, haar_embed)]
-            # Stack embeddings for the current instance: shape (dim_seq, dim_model)
-            final_embeddings.append(torch.stack(instance_embeddings, dim=0))
-    
-        # Stack all instance embeddings: shape (batch_size, dim_seq, dim_model)
-        return torch.stack(final_embeddings, dim=0)
+        return batchts_embebed, att_mask
 
 
 
--- hitsbe/models/hitsBERT.py
+++ hitsbe/models/hitsBERT.py
@@ -1,63 +1,193 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
-from transformers import BertForSequenceClassification, BertTokenizer
+from transformers import BertConfig, BertModel
 
-from hitsbe.hitsbe import Hitsbe
+from hitsbe import Hitsbe
 
-class HitsBERTClassifier(nn.Module):
-    def __init__(self, batch_size, sequence_length=128, num_classes=3, bert_model_name='bert-base-uncased'):
-        super(HitsBERTClassifier, self).__init__()
-        # Load BERT for sequence classification with hidden states output
-        self.bert = BertForSequenceClassification.from_pretrained(
-            bert_model_name, output_hidden_states=True
-        )
+import os
 
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        cls_token = self.tokenizer.cls_token  
-        cls_token_id = self.tokenizer.convert_tokens_to_ids(cls_token)
-        with torch.no_grad():
-            self.cls_embedding = self.bert.bert.embeddings.word_embeddings(
-                torch.tensor([cls_token_id])
-            )
+class HitsBERT(nn.Module):
+    def __init__(self, bert_config = None, hitsbe= None):
+        super().__init__()
+        # Load BERT model via BertConfig     
+        # El comentario de abajo deberia estar en la memoria o en el README como tutorial de uso
+        # If we want to use "bert-base-uncased" or "bert-large-uncased"
+        # we must define the BertConfig externally using `BertConfig.from_pretrained(...)`
+        # and load pretrained weights separately using `BertModel.from_pretrained(...).state_dict()`
+        # Example: 
+        # bert = BertModel.from_pretrained("bert-base-uncased")
+        # config = bert.config
+        # model = HitsBERT(bert_config=config)
+        # model.bert.load_state_dict(bert.state_dict())
+        self.config = bert_config or BertConfig()
+        self.bert = BertModel(self.config)
         
-        # Instantiate the Hitsbe module
-        self.hitsbe = Hitsbe()
-        self.batch_size = batch_size
-        self.sequence_length = sequence_length
+        # Instantiate the Hitsbe module.
+        self.hitsbe = hitsbe or Hitsbe()
         
-        # Classification layer: the [CLS] vector (first position) is used to predict the class
-        self.classifier = nn.Linear(768, num_classes)
+        # Initialize the classifier token [CLS]. It must be trainable
+        self.cls_embedding = nn.Parameter(torch.randn(1, 1, self.config.hidden_size))
+        nn.init.normal_(self.cls_embedding, mean=0.0, std=self.config.initializer_range)
+    
+    # Prepend [CLS] token to each sequence
+    # embeds (batch_size, dim_seq, hidden_size)
+    # att_mask (batch_size, dim_seq) 
+    def concat_cls(self, embeds, att_mask):
+        batch_size = embeds.size(0)
 
-    def forward(self, data, attention_mask=None):
-        # Obtain the HitsBE embeddings
-        hitsbe_embeddings = self.hitsbe.get_embedding(data) # (batch_size, sequence_length, hidden_size)
-        
-        # Prepend [CLS] token to each sequence
-        cls = self.cls_embedding.to(hitsbe_embeddings.device)  # (1, hidden_size)
-        # unsqueeze(0): (1,1,hidden_size)
-        # expand(hitssebe_embedding.size(0), -1, -1): (batch_size, 1, hidden_size)
-        cls = cls.unsqueeze(0).expand(hitsbe_embeddings.size(0), -1, -1)  
+        # expand(batch_size, -1, -1): (batch_size, 1, hidden_size)
+        cls = self.cls_embedding.expand(batch_size, -1, -1)
         # Concatenate cls and hitsbe_embedding on the dimension 1
-        embeddings_with_cls = torch.cat((cls, hitsbe_embeddings), dim=1)  # (batch_size, sequence_length + 1, hidden_size)
+        embeddings_with_cls = torch.cat((cls, embeds), dim=1)  # (batch_size, dim_seq + 1, hidden_size)
         
-        # Adjust the attention mask
-        if attention_mask is None:
-            internal_mask = self.hitsbe.att_mask.to(embeddings_with_cls.device) # (seq_length)
-            # Concatenate ones (batch_size,1) with (1, seq,length + 1) ot dim 1, (batch_size, se_length + 1)
-            full_mask = torch.cat((torch.ones(embeddings_with_cls.size(0), 1, device=embeddings_with_cls.device),
-                                    internal_mask.unsqueeze(0).expand(embeddings_with_cls.size(0), -1)), dim=1)
-            attention_mask = full_mask
-        else:
-            attention_mask = attention_mask.to(embeddings_with_cls.device)
+        cls_mask = torch.ones((batch_size, 1), dtype=att_mask.dtype, device=att_mask.device)
+        # (batch_size, dim_seq + 1) 
+        att_mask_cls =  torch.cat((cls_mask, att_mask), dim=1)
         
-        outputs = self.bert(inputs_embeds=embeddings_with_cls, attention_mask=attention_mask)
-        # Get the last hidden state
-        last_hidden = outputs.hidden_states[-1]  # (batch_size, sequence_length + 1, hidden_size)
-        # Extract the [CLS] vector 
-        cls_vectors = last_hidden[:, 0, :]  # (batch_size, hidden_size)
-        # Calculate the classification logits
-        logits = self.classifier(cls_vectors)  # (batch_size, num_classes)
+        return embeddings_with_cls, att_mask_cls
+
+    def forward(self, data):        
+        # Obtain the HitsBE embeddings
+        hitsbe_embeddings, att_mask = self.hitsbe.get_embedding(data) # (batch_size, dim_seq, hidden_size)
+                
+        embeddings_cls, att_mask_cls = self.concat_cls(hitsbe_embeddings, att_mask)
+                        
+        return self.bert(inputs_embeds=embeddings_cls, attention_mask=att_mask_cls)
+    
+    def save_pretrained(self, path, filename = "hitsbert_model.bin"):
+        os.makedirs(path, exist_ok =True)
+        torch.save(self.state_dict(), os.path.join(path, filename))
+        self.config.to_json_file(os.path.join(path, "config.json"))
+
+    @classmethod
+    def from_pretrained(cls, path, filename = "hitsbert_model.bin", hitsbe=None):
+        bert_config = BertConfig.from_json_file(os.path.join(path, "config.json"))
+        model = cls(bert_config=bert_config, hitsbe=hitsbe)
+        # map_location load "cpu" if the saved default is not available
+        model.load_state_dict(torch.load(os.path.join(path, filename), map_location="cpu"))
+        return model
+    
+
+class HitsBERTPretraining(nn.Module):
+    def __init__(self, model: HitsBERT):
+        super().__init__()
+
+        self.model = model
+
+        self.mask_token = nn.Parameter(torch.randn(1, 1, self.model.config.hidden_size))
+        # Weights initialized with 0 mean and initializer_range std
+        nn.init.normal_(self.mask_token, mean=0.0, std=self.model.config.initializer_range)
+
+    # Cuidado, X contiene las series sin repetir y MLM contiene num_repeat
+    # mascaras por cada serie. Esto se hace para optimizar hitsbe.
+    # MLM: tensor torch binario con 1 en [MASK] (num_iter, dim_seq, 1)
+    # X: time serie (batch_size, ts_len)
+    # options: (num_iter, num_options, seq_length, dim_model)
+    def forward(self, X, MLM, options):
+        device = next(self.parameters()).device
+
+        X = X.to(device)
+        MLM = MLM.to(device)
+        options = options.to(device)
         
+        num_iter = MLM.size(0)
+        num_repeat = num_iter//X.size(0)
+        # Revisar dimensiones
+
+        # (1, dim_seq, dim_model)
+        X_embed, att_mask = self.model.hitsbe.get_embedding(X)
+
+        # (num_iter, dim_seq, dim_model)
+        embeddings_repeated = X_embed.repeat_interleave(num_iter, dim=0)
+        attention_mask = att_mask.repeat_interleave(num_iter, dim=0)
+        
+        # (num_iter, dim_seq, dim_model)
+        masktoken_matrix = self.mask_token.expand(num_iter, MLM.size(1), -1)
+
+        # (num_iter, dim_seq, 1)
+        MLM_comp = 1 - MLM
+
+        # Replace the corresponding mask index by [MASK] token
+        # (num_iter, dim_seq, dim_model)
+        embeddings_masked = embeddings_repeated*MLM_comp.unsqueeze(2) + masktoken_matrix*MLM.unsqueeze(2)
+        
+        # (num_iter, dim_seq + 1(CLS), dim_model)
+        embeddings_cls, attention_mask_cls = self.model.concat_cls(embeddings_masked, attention_mask)
+
+        # (num_iter, dim_seq + 1(CLS), dim_model)                
+        output = self.model.bert(inputs_embeds=embeddings_cls, attention_mask=attention_mask_cls)
+
+        # Remove [CLS] token 
+        last_hidden_state = output.last_hidden_state
+        answers = last_hidden_state[:, 1:, :]
+
+        # answers dimension: (num_iter, seq_length, dim_model);             
+        # options dimension: (num_iter, num_options, seq_length, dim_model)
+        # scalar product of the words
+        # (num_iter, num_options, seq_length) 
+        # For each element, we have n options, each one with seq_length scalar product of the words
+        token_scores = torch.einsum('bij,bkij->bki', answers, options)
+
+        # (num_iter, num_options)
+        # Sum the results of the scalar product in each option
+        logits = token_scores.sum(dim = 2)
+
+        return logits # Each row contains the logits for each task
+    
+    def save_pretrained(self, path, filename="hitsbert_pretraining.bin"):
+        os.makedirs(path, exist_ok=True)
+        torch.save(self.state_dict(), os.path.join(path, filename))
+        self.model.config.to_json_file(os.path.join(path, "config.json"))
+
+    @classmethod
+    def from_pretrained(cls, path, filename="hitsbert_pretraining.bin"):
+        config = BertConfig.from_json_file(os.path.join(path, "config.json"))
+        model = HitsBERT(config)
+        pretrain = cls(model)
+        pretrain.load_state_dict(torch.load(os.path.join(path, filename), map_location="cpu"))
+        return pretrain
+
+# REVISAR BIEN
+class HitsBERTClassifier(nn.Module):
+    def __init__(self, model: HitsBERT, num_classes: int):
+        super().__init__()
+        self.model = model
+        self.num_classes = num_classes
+
+        self.classifier = nn.Linear(self.model.config.hidden_size, num_classes)
+
+        nn.init.normal_(self.classifier.weight, mean=0.0, std=self.model.config.initializer_range)
+        if self.classifier.bias is not None:
+            nn.init.zeros_(self.classifier.bias)
+
+    def forward(self, X):
+        # X: (batch_size, ts_len)
+        hitsbe_embeddings, att_mask = self.model.hitsbe.get_embedding(X)
+        embeddings_cls, att_mask_cls = self.model.concat_cls(hitsbe_embeddings, att_mask)
+
+        output = self.model.bert(inputs_embeds=embeddings_cls, attention_mask=att_mask_cls)
+        cls_embedding = output.last_hidden_state[:, 0]  # (batch_size, hidden_size)
+
+        logits = self.classifier(cls_embedding)  # (batch_size, num_classes)
         return logits
+
+    def save_pretrained(self, path, filename="hitsbert_classifier.bin"):
+        os.makedirs(path, exist_ok=True)
+        torch.save({
+            "state_dict": self.state_dict(),
+            "num_classes": self.num_classes,
+        }, os.path.join(path, filename))
+        self.model.config.to_json_file(os.path.join(path, "config.json"))
+
+    @classmethod
+    def from_pretrained(cls, path, filename="hitsbert_classifier.bin", hitsbe=None):
+        config = BertConfig.from_json_file(os.path.join(path, "config.json"))
+        hitsbert = HitsBERT(bert_config=config, hitsbe=hitsbe)
+
+        checkpoint = torch.load(os.path.join(path, filename), map_location="cpu")
+        classifier = cls(hitsbert, num_classes=checkpoint["num_classes"])
+        classifier.load_state_dict(checkpoint["state_dict"])
+        return classifier
+
+
